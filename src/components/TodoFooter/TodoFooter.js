a19e8c9972b5a021273f99a04f8f657e0412b916
Nicky <nickysauceofficial@gmail.com>
2023-02-26 09:32:29 +0530

    todo footer tests

--- src/components/TodoFooter/TodoFooter.js
+++ src/components/TodoFooter/TodoFooter.js
@@ -7,7 +7,7 @@ function TodoFooter({ numberOfIncompleteTasks }) {
     <div className="todo-footer">
       <p>
         {numberOfIncompleteTasks}{" "}
-        {numberOfIncompleteTasks === 1 ? "task" : "tasks"} left
+        {numberOfIncompleteTasks <= 1 ? "task" : "tasks"} left
       </p>
       <Link to="/followers">Followers</Link>
     </div>
